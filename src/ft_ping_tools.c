2157a8295117c1efcb5938592f6cd837c26e4567
phnguyen <phnguyen@student.42.fr>
2023-10-17 15:14:58 +0200

    test

--- src/ft_ping_tools.c
+++ src/ft_ping_tools.c
@@ -23,29 +23,52 @@ unsigned short	checksum(void *b, int len)
 	result = ~sum;
 }
 
-// Resolves the reverse lookup of the hostname
-// char* reverse_dns_lookup(char *ip_addr)
-// {
-// 	struct sockaddr_in temp_addr;   
-// 	socklen_t len;
-// 	char buf[NI_MAXHOST], *ret_buf;
 
-// 	if (inet_pton(AF_INET, ip_addr, void *restrict dst) == 0) {
-// 		printf("Could not resolve reverse lookup of hostname\n");
-// 		return NULL;
-// 	}
-
-// 	temp_addr.sin_family = AF_INET;
-// 	temp_addr.sin_addr.s_addr = inet_addr(ip_addr);
-// 	len = sizeof(struct sockaddr_in);
+// Performs a DNS lookup
+char *dns_lookup(char *addr_host, struct sockaddr_in *addr_con)
+{
+    printf("\nResolving DNS..\n");
+    struct hostent *host_entity;
+    char *ip=(char*)malloc(NI_MAXHOST*sizeof(char));
+    int i;
  
-// 	if (getnameinfo((struct sockaddr *) &temp_addr, len, buf,
-// 					sizeof(buf), NULL, 0, NI_NAMEREQD))
-// 	{
-// 		printf("Could not resolve reverse lookup of hostname\n");
-// 		return NULL;
-// 	}
-// 	ret_buf = (char*)malloc((strlen(buf) +1)*sizeof(char) );
-// 	strcpy(ret_buf, buf);
-// 	return ret_buf;
-// }
+    if ((host_entity = gethostbyname(addr_host)) == NULL)
+    {
+        // No ip found for hostname
+        return NULL;
+    }
+     
+    //filling up address structure
+    strcpy(ip, inet_ntoa(*(struct in_addr *)
+                          host_entity->h_addr));
+ 
+    (*addr_con).sin_family = host_entity->h_addrtype;
+    (*addr_con).sin_port = htons (PORT_NO);
+    (*addr_con).sin_addr.s_addr  = *(long*)host_entity->h_addr;
+ 
+    return ip;
+     
+}
+ 
+// Resolves the reverse lookup of the hostname
+char* reverse_dns_lookup(char *ip_addr)
+{
+    struct sockaddr_in temp_addr;   
+    socklen_t len;
+    char buf[NI_MAXHOST], *ret_buf;
+ 
+    temp_addr.sin_family = AF_INET;
+    temp_addr.sin_addr.s_addr = inet_addr(ip_addr);
+    len = sizeof(struct sockaddr_in);
+ 
+    if (getnameinfo((struct sockaddr *) &temp_addr, len, buf,
+                    sizeof(buf), NULL, 0, NI_NAMEREQD))
+    {
+        printf("Could not resolve reverse lookup of hostname\n");
+        return NULL;
+    }
+    ret_buf = (char*)malloc((strlen(buf) +1)*sizeof(char) );
+    strcpy(ret_buf, buf);
+    return ret_buf;
+}
+ 
